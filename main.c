63548ce67b25d8376829cc705053b100068b28d5
kvasa <kvasan2104@gmail.com>
2023-06-20 09:17:49 +0300

    argparser added

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int get_random_value() {
+    return rand() % 2;
+}
+
+int main(int argc, char **argv) {
+    int opt;
+    int nodes; // 2^64
+    int edges; // 2^64
+    bool adjacency_flag = false;
+    bool incidence_flag = false;
+    while ((opt = getopt(argc, argv, "n:e:ai")) != -1) {
+        switch (opt) { //Ключи количества узлов и ребер обязательные,
+                       //ключи матриц можно задать либо одну, либо обе. Сделать проверки на это.
+            case 'n':
+                nodes = atoi(optarg);
+                break;
+            case 'e':
+                edges = atoi(optarg);
+                break;
+            case 'a':
+                adjacency_flag = true;
+                break;
+            case 'i':
+                incidence_flag = true;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-n: number of nodes] [-e: number of edges]"
+                                "[-a generate adjacency matrix] [-i generate incidence matrix]", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    return EXIT_SUCCESS;
 }
